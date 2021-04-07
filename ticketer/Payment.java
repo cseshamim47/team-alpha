16b54cbe72f885f3213191d6c789f2faf280360e
Yaarian <cold.blooded.murderer0320@gmail.com>
2021-04-08 00:32:03 +0600

    8th April, 12:31

--- ticketer/Payment.java
+++ ticketer/Payment.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class Payment 
 {
     String accountNumber;
+    Payment()
+    {
+        /*
+        System.out.println("Choose Payment Method -->");
+        System.out.println("1. Bkash");
+        System.out.println("2. Rocket");
+        System.out.println("3. Credit/Visa");
+        System.out.print("Enter Your Choice : ");
+        */
+    }
     
     Payment(int choice)
     {
@@ -19,9 +29,16 @@ public class Payment
                 System.out.print("Enter BKash Number : ");
                 accountNumber = input.nextLine();
                 
+                 
+                System.out.println("Confirm? (Y/N)");
+                String confirm = input.nextLine();
+                confirm = confirm.toLowerCase();
+                if(confirm.equals("y"))
+                {
                 System.out.println("-----------------------------------");
                 System.out.println("|        PAYMENT SUCCESSFUL !!!    | ");
                 System.out.println("-----------------------------------");
+                }
     
             }
             
@@ -31,9 +48,16 @@ public class Payment
                 System.out.print("Enter Rocket Number : ");
                 accountNumber = input.nextLine();
                 
+                 
+                System.out.println("Confirm? (Y/N)");
+                String confirm = input.nextLine();
+                confirm = confirm.toLowerCase();
+                if(confirm.equals("y"))
+                {
                 System.out.println("-----------------------------------");
                 System.out.println("|        PAYMENT SUCCESSFUL !!!    | ");
                 System.out.println("-----------------------------------");
+                }
     
             }
             
@@ -43,9 +67,16 @@ public class Payment
                 System.out.print("Enter Credit Card Number : ");
                 accountNumber = input.nextLine();
                 
+                System.out.println("Confirm? (Y/N)");
+                String confirm = input.nextLine();
+                confirm = confirm.toLowerCase();
+                if(confirm.equals("y"))
+                {
+                
                 System.out.println("-----------------------------------");
                 System.out.println("|        PAYMENT SUCCESSFUL !!!    | ");
                 System.out.println("-----------------------------------");
+                }
     
             }
             default:{System.out.println("Choose A Valid Option (1-3)");}
--- ticketer/Start.java
+++ ticketer/Start.java
@@ -1,25 +1,31 @@
-
 package ticketer;
+
 import java.util.Scanner;
 import java.io.*;
-public class Start 
-{
-    public static void cls() 
-    {
+
+/*
+BUG IN RESERVATION LINE 30
+BUG IN MAIN METHOD LINE 60
+BUG IN RESERVATION LINE 43
+*/
+
+
+
+public class Start {
+
+    public static void cls() {
         /// For Clear Screen
-        try 
-        {/// indicates from /c for cmd inheriting the function "cls";
+        try {/// indicates from /c for cmd inheriting the function "cls";
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        } 
-        catch (Exception e) 
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
-    public static void main(String[] args) 
-    {
-        
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         /*
         1. Login
         2. Registration
@@ -31,17 +37,35 @@ public class Start
            ii) Train
           iii) Plane
            iv) Ship
-        5. Confirmation
-        6. Payment
+        5. Cost Calculation
+        6. Confirmation Payment
         7. Print Ticket
-        */
-        
-       
-        
-        
+         */
+        Reservation r = new Reservation();
+        cls();
+        System.out.println("Choose Your Root --> ");
+        System.out.println("1. Bus");
+        System.out.println("2. Train");
+        System.out.println("3. Plane");
+        System.out.println("4. Ship");
+        System.out.print("Enter Your Choice : ");
+        int root = input.nextInt();
+        cls(); 
+        ChooseRoot cr = new ChooseRoot(root);
+        cls();
+        CostCalculation cc = new CostCalculation();
+
+        System.out.println("Choose Payment Method -->");
+        System.out.println("1. Bkash");
+        System.out.println("2. Rocket");
+        System.out.println("3. Credit/Visa");
+        System.out.print("Enter Your Choice : ");
+        int choice = input.nextInt();
+        //cls();
+        Payment p = new Payment(choice);
+        cls();
+        PrintTicket pt = new PrintTicket();
+
     }
- 
-    
-    
-    
+
 }
